5ee6088d13712e3e9dd46d18f55ded80763b6f9a
araceliaragon <aracelia8661@tywls.org>
2016-03-16 21:05:48 +0000

    First commit

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
+      $(document).ready(function() {
+      
+       
+       $("#playButton").click(play);
+       
+      });
+      
+      
+      
+      function getRandomPlay(){
+        var outcomeArray = ["Rock", "Paper", "Scissors"];
+        return outcomeArray[ Math.floor(Math.random() * outcomeArray.length) ];
+      }
+      
+      function resetFaces(){
+        $(".girl").removeClass("girl-lose"); 
+        $(".boy").removeClass("boy-lose");  
+      }
+     
+      
+      function play(){
+        
+        resetFaces();
+        
+        var boyPlayed = getRandomPlay();
+        var girlPlayed = getRandomPlay();
+        $("#girlResult").html(boyPlayed);
+        $("#boyResult").html(girlPlayed);
 
-//  STEP ONE: 
-//  Attach a click event listener to the answer button that will
-//  execute the checkAnswer function.
-
-$(document).ready(function() {
-
-  // attach your button click event listener here
-
-});
-
-function checkAnswer(){
-  
-  var answer =  $("input[name=question-one]:checked").val();
-  //input[name='ans3']:checked
-  console.log(answer);
-  
-  
-  // STEP TWO:
-  // with an if/else statement, check to see if the answer is correct      
-  // if the answer is correct, use jQuery to add the class "question-correct" to the
-  // div with the id of "question-one", otherwise, add the class "question-incorrect"
-  
-  
-  
-  
-  // STEP THREE:
-  // use a jQuery effect to slideDown the appropriate "remark-correct" or "remark-incorrect"
-  
-  
-  
-  
-  // STEP FOUR (extra credit):
-  // use jQuery to display the reset button once the user has made a selection (this code should be outside of the if/else statement)
-  // create a function called resetQuiz that will remove any styles that we added
-  // create an click event listener for the reset button and attach it to the resetQuiz function
-  
-}
+        if(boyPlayed == girlPlayed){
+        
+          tie(); 
+        
+        } else{
+          if(boyPlayed == "Rock" && girlPlayed == "Scissors"){
+            boyWins();
+          }
+          else if (boyPlayed =="Paper" && girlPlayed == "Rock"){
+            boyWins();
+          }
+          else if (boyPlayed == "scissors" && girlPlayed == "Paper"){
+            boyWins();
+          }
+          else {
+            girlWins();
+          }
+        }
+      }  
+         
+      function tie(){
+        $("#message").html("It's a tie. Try again.");  
+      }
+      
+      function boyWins(){
+        $("#message").html("Boy wins!");
+        $(".girl").addClass("girl-lose")
+      }
+      
+      function girlWins(){
+        $("#message").html("Girl wins!");
+        $(".boy").addClass("boy-lose")
+      }
